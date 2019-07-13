ee3f57d1e4314aa9bdd7ee5423bb61e3b6dfe270
Patrick Bergeron <pbergeron@lunix.pro>
2019-07-13 12:26:34 -0400

    remove the ngNote bugged folder.

--- ngNote/src/app/class/common.ts
+++
@@ -1,9 +0,0 @@
-import { Injector } from '@angular/core';
-
-export let InjectorInstance: Injector;
-export const ApiUrl: string = "http://localhost:8080/api";
-
-export function InitInjector(injector: Injector) {
-    if (InjectorInstance == null)
-        InjectorInstance = injector
-}
--- ngNote/src/app/class/httpstatus.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HTTPStatus } from './httpstatus';
-
-describe('HTTPStatus', () => {
-  it('should create an instance', () => {
-    expect(new HTTPStatus()).toBeTruthy();
-  });
-});
