e13be18d0eeeb328d8fc063d432c542c6370aff3
Patrick Bergeron <pat.bergeron@cgi.com>
2019-07-22 12:58:59 -0400

    UserToken fix Fix a an error when the GetUserToken function would get no result.

--- src/gonote/mngment/usertoken.go
+++ src/gonote/mngment/usertoken.go
@@ -10,15 +10,17 @@ import (
 )
 
 // GetUserToken fetch in the database the token associated with a specified user.
+//
 // `t` is the token string.
-// `u` is the username owning the token
+// `u` is the user id of the user owning the token.
 // `c` is an optional database connection.
+//
 // Returns the user token (r) respecting the constraints.
 func GetUserToken(t string, u string, c *db.Conn) (r *UserToken) {
 	db.MustConnect(c, func(c *db.Conn) {
 		p := []interface{}{u, t}
-		rst, _, err := db.Run(c, userTokenGetQuery, p, reflect.TypeOf(UserToken{}))
-		if err == nil {
+		rst, cnt, err := db.Run(c, userTokenGetQuery, p, reflect.TypeOf(UserToken{}))
+		if err == nil && cnt > 0 {
 			r = rst[0].(*UserToken)
 		}
 	})
