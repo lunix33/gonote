9b7c3fd08955943269d5c1550a2e123f3dd98d92
Patrick Bergeron <pbergeron@lunix.pro>
2019-07-30 23:16:48 -0400

    #2 Add API to get specific user.

---
+++ src/gonote/router/rtedef_user.go
@@ -0,0 +1,44 @@
+package router
+
+import (
+	"gonote/db"
+	"gonote/mngment"
+	"net/http"
+)
+
+const userRteAddr = "^/user/?{id}?$"
+
+type userRteHandler struct{}
+
+func (userRteHandler) Get(rw *http.ResponseWriter, req *http.Request, r *Route) {
+	var id = r.Params["id"]
+
+	if id != "" {
+		// We can get a user from the database with the ID.
+
+		// Get the User.
+		var user *mngment.User
+		db.MustConnect(nil, func(c *db.Conn) {
+			user = mngment.GetUserByID(id, c)
+		})
+
+		if user != nil {
+			// If the current user isn't an admin, secure the user details.
+			if !r.User.IsAdmin {
+				user.Password = ""
+				user.Email = ""
+			}
+
+			WriteJSON(rw, user)
+			return
+		}
+	} else if id == "" && r.User != nil {
+		// We don't have the ID of a user, but we have an authenticated user.
+		// Return the information about the current user.
+
+		WriteJSON(rw, r.User)
+		return
+	}
+
+	NotFound(rw)
+}
